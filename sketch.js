daa65dded2977f1689cfde99b584c3d9db4559b0
nikolasaratlija <31618346+nikolasaratlija@users.noreply.github.com>
2021-09-29 20:12:48 +0200

    new images

--- sketch.js
+++ sketch.js
@@ -1,34 +1,34 @@
 let bg;
-let bench;
+let table;
 let tree;
 let slider;
 let objects = []
 
 function preload() {
     bg = loadImage('assets/bg.png')
-    bench = loadImage('assets/bench.png');
-    tree = loadImage('assets/tree.png');
+
+    table = loadImage('assets/elements/picknicktafel.png');
+    tree = loadImage('assets/elements/Boom.png');
 }
 
 function setup() {
     createCanvas(750, 750);
 
+    // slider for scale
     slider = createSlider(0, 100, 100);
     slider.position(750, 250);
     slider.style('width', '120px');
-
-    bench.resize(200, 0)
-    tree.resize(150, 0)
 
     document.querySelectorAll('.menu > img')
         .forEach(el => el.addEventListener('click', e => {
             let object;
 
-            if (el.src.includes('tree'))
+            if (el.src.includes('Boom'))
                 object = tree
-            else if (el.src.includes('bench'))
-                object = bench
+            else if (el.src.includes('picknicktafel'))
+                object = table
 
+            object.resize(200, 0)
             objects.push(new Draggable(object, 300, 300, object.width, object.height))
         }))
 }
