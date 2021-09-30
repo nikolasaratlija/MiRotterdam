c9e34f806e7ced86447a449a66057d940d9e5cc1
nikolasaratlija <31618346+nikolasaratlija@users.noreply.github.com>
2021-09-30 21:06:58 +0200

    test

--- scripts/get-images.php
+++ scripts/get-images.php
@@ -1,9 +1,8 @@
 const files =
 <?php
-echo 5;
-//$out = array();
-//foreach (glob('..\assets\elements\*.png') as $filename) {
-//    $out[] = "assets\/elements\/" . basename($filename);
-//}
-//echo json_encode($out);
+$out = array();
+foreach (glob('..\assets\elements\*.png') as $filename) {
+    $out[] = "assets\/elements\/" . basename($filename);
+}
+echo json_encode($out);
 ?>
