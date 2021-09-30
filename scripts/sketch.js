60bb72c406191fb4f3705dde90f7dd2531709c92
nikolasaratlija <31618346+nikolasaratlija@users.noreply.github.com>
2021-09-30 20:49:54 +0200

    comments

--- scripts/sketch.js
+++ scripts/sketch.js
@@ -1,24 +1,25 @@
 let bg;
 
-let table;
-let tree;
-
 let slider;
 
 let loadedImages = []
 let canvasImages = []
 
 function preload() {
+    // background for canvas
     bg = loadImage('assets/bg.png')
 
+    // 1. iterate over all image filenames and load images into p5 canvas
+    // 2. creates an <img> element for each image and inserts it into the menu
     files.forEach(file => {
+        // populates `loadedImages` array with loaded p5 images
         const parts = file.split('\/')
         loadedImages[parts[parts.length - 1]] = loadImage(file)
 
+        // creates <img> elements
         let image = document.createElement('img')
         image.src = file
         image.width = 100
-
         document.querySelector('.menu').appendChild(image)
     })
 }
@@ -26,18 +27,22 @@ function preload() {
 function setup() {
     createCanvas(750, 750);
 
-    // slider for scale
+    // slider for adjusting the scale of an element
     slider = createSlider(0, 100, 100);
     slider.position(750, 250);
     slider.style('width', '120px');
 
+    // click event listener on each <img> in the menu.
     document.querySelectorAll('.menu > img')
         .forEach(el => el.addEventListener('click', e => {
-            const parts = e.target.src.split('\/')
-            const search = parts[parts.length - 1]
-            let object = loadedImages[search]
+            // looks at the src of the clicked <img>
+            // finds a loaded image in `loadedImages` by key
+            const imageSource = e.target.src.split('\/')
+            const imageName = imageSource[imageSource.length - 1]
+            let object = loadedImages[imageName]
 
             object.resize(200, 0)
+            // push image into canvas
             canvasImages.push(new Draggable(object, 300, 300, object.width, object.height))
         }))
 }
