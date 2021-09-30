cb495373dfbe0d4b2a3e07d0b5c249350aadb85e
nikolasaratlija <31618346+nikolasaratlija@users.noreply.github.com>
2021-09-30 21:11:27 +0200

    forwardslashes

--- scripts/get-images.php
+++ scripts/get-images.php
@@ -1,8 +1,8 @@
 const files =
 <?php
 $out = array();
-foreach (glob('..\assets\elements\*.png') as $filename) {
-    $out[] = "assets\/elements\/" . basename($filename);
+foreach (glob('../assets/elements/*.png') as $filename) {
+    $out[] = "assets/elements/" . basename($filename);
 }
 echo json_encode($out);
 ?>
