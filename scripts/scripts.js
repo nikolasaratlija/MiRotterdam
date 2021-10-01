5e2847a8135c9d852dec6b9c0ad3b8d1f555fe6f
nikolasaratlija <31618346+nikolasaratlija@users.noreply.github.com>
2021-10-01 22:24:40 +0200

    fixed issue with onclick events on objects

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,32 +5,36 @@ files.forEach(file => {
 
     image.addEventListener('click', (e) => {
         const img = e.target.cloneNode(true)
+        const randId = 'obj' + Math.floor(Math.random() * 99) // assign random id to object
+        img.id = randId
         document.getElementById('container').appendChild(img)
 
-        Draggable.create('#container img', {
+        Draggable.create(`#container img#${randId}`, {
             type: 'x,y', bounds: '#container', onClick: function () {
-                this.target.classList.add('highlighted')
+                const el = this.target
+                if (el.classList.contains('highlighted'))
+                    el.classList.remove('highlighted')
+                else
+                    el.classList.add('highlighted')
             }
         });
     })
-
     document.querySelector('.menu').appendChild(image)
 })
 
-// menu-button
-// animation
-let tween = gsap.to('.menu', {duration: 0.2, left: 0, display: 'block'})
-tween.pause()
+(function () { // menu-button
+    let tween = gsap.to('.menu', {duration: 0.2, left: 0, display: 'block'}) // GSAP animation
+    tween.pause()
 
-let isMenuShown = false
-document.getElementById('menu-button').addEventListener('click', e => {
-    if (!isMenuShown)
-        tween.play()
-    else
-        tween.reverse()
-
-    isMenuShown = !isMenuShown
-})
+    let isMenuShown = false
+    document.getElementById('menu-button').addEventListener('click', e => {
+        if (!isMenuShown)
+            tween.play()
+        else
+            tween.reverse()
+        isMenuShown = !isMenuShown
+    })
+})()
 
 // screenshot
 document.addEventListener('keypress', (e) => {
