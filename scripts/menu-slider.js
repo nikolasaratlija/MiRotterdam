0414f077d67a19bea5bb31724e412853dcb8bee6
nikolasaratlija <31618346+nikolasaratlija@users.noreply.github.com>
2021-10-03 23:37:23 +0200

    better animation speed

--- scripts/menu-slider.js
+++ scripts/menu-slider.js
@@ -1,7 +1,7 @@
 (() => {
     let animation = gsap.timeline()
     animation.to('.menu-container', {duration: 0.2, left: 0})
-    animation.to('.menu-button', {duration: 0.1, rotation: 180})
+    animation.to('.menu-button', {duration: 0.2, rotation: 180})
     animation.pause()
 
     let isMenuShown = false
