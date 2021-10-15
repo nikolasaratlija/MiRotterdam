0719bf3caf5a428968badfce1362881fc520678b
nikolasaratlija <31618346+nikolasaratlija@users.noreply.github.com>
2021-10-16 01:05:28 +0200

    sliding menus

--- public/scripts/menu-slider.js
+++ public/scripts/menu-slider.js
@@ -1,23 +1,47 @@
 (() => {
-    let animation = gsap.timeline()
-    animation.to('.elements-menu-container', {duration: 0.2, left: 0})
-    animation.pause()
+    const canvas = document.getElementById('canvas')
 
-    let isMenuShown = false
+    // element menu
+    let isElementMenuShown = false
+    const elementMenuAnimation = gsap.timeline()
+    elementMenuAnimation.to('.elements-menu-container', {duration: 0.2, left: 0})
+    elementMenuAnimation.pause()
 
     document.getElementById('elements-menu-button').addEventListener('click', e => {
-        if (!isMenuShown)
-        {
-            animation.play()
-            isMenuShown = !isMenuShown
+        if (!isElementMenuShown) {
+            elementMenuAnimation.play()
+            isElementMenuShown = !isElementMenuShown
         }
     })
 
-    document.getElementById('canvas').addEventListener('click', e => {
-        if (isMenuShown)
-        {
-            animation.reverse()
-            isMenuShown = !isMenuShown
+    canvas.addEventListener('click', e => {
+        if (isElementMenuShown) {
+            elementMenuAnimation.reverse()
+            isElementMenuShown = !isElementMenuShown
+        }
+    })
+
+    // attribute editor
+    const elementEditorButton = document.getElementById('menu-button')
+    let isElementEditorShown = false
+    const elementEditorAnimation = gsap.timeline()
+    elementEditorAnimation.to('.element-editor', {duration: 0.2, top: 0})
+    elementEditorAnimation.pause()
+
+    elementEditorButton.addEventListener('click', e => {
+        if (!isElementEditorShown) {
+            elementEditorAnimation.play()
+            isElementEditorShown = !isElementEditorShown
+        } else {
+            elementEditorAnimation.reverse()
+            isElementEditorShown = !isElementEditorShown
+        }
+    })
+
+    canvas.addEventListener('click', e => {
+        if (isElementEditorShown) {
+            elementEditorAnimation.reverse()
+            isElementEditorShown = !isElementEditorShown
         }
     })
 })()
