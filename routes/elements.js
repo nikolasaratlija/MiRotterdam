6e6c7e3781f470cd119cbe136447f6e2e942e1e5
nikolasaratlija <31618346+nikolasaratlija@users.noreply.github.com>
2021-10-15 16:58:04 +0200

    removed unused file

--- routes/elements.js
+++ routes/elements.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const fs = require('fs')
-const path = require("path");
+const path = require('path');
 
 router.get('/', (req, res) => {
     let filesArr = []
