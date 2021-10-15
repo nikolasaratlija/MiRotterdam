d7dd3221a7c70d673c7ad12807df72ad1a1695c9
nikolasaratlija <31618346+nikolasaratlija@users.noreply.github.com>
2021-10-15 17:10:24 +0200

    separated index and design-maker

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', function (req, res) {
+router.get('/', (req, res) => {
     res.render('index', {
+        title: 'MiRotterdam',
+        bodyClass: 'index'
+    })
+});
+
+router.get('/ontwerpomgeving', (req, res) => {
+    res.render('design-maker', {
         title: 'Ontwerpomgeving',
         bodyClass: 'design-maker'
     })
-});
+})
 
 module.exports = router;
