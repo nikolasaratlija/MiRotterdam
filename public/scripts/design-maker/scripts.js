2bb80705d1d86d5a005770cf1732a916ac860ecf
nikolasaratlija <saratlijanikola@gmail.com>
2021-12-08 23:21:59 +0100

    comment most scripts

--- public/scripts/design-maker/scripts.js
+++ public/scripts/design-maker/scripts.js
@@ -1,24 +1,30 @@
-import {ElementsMenu} from "./elements-library/ElementsMenu.js";
-import {hideElementsMenu} from "./elements-library/animation.js";
+import {showElementsMenu} from "./elements-library/animation.js";
 
-import * as canvas from "./canvas/Canvas.js"
-import {setImage as setCanvasImage} from "./canvas/setImage.js";
-
-import {AttributeEditor} from "./attribute-editor/AttributeEditor.js"
-import {SizeSlider} from "./attribute-editor/ElementSizeSlider.js"
-
-import {BottomMenu} from "./buttons/BottomMenu.js";
-
-
-// Loads element-library with elements, and sets click events on each element to show them on canvas
-ElementsMenu.loadElements(element => {
-    canvas.addElement(element)
-    hideElementsMenu()  // hide menu when an user adds element
+document.getElementById('elements-menu-button').addEventListener('click', () => {
+    showElementsMenu()
 })
 
-setCanvasImage() // Sets the canvas image
-
-AttributeEditor() // Initializes the attribute-editor menu
-SizeSlider(canvas.imageWidth) // Initializes size-slider
-
-BottomMenu() // Initializes buttons of bottom menu
+// import {ElementsMenu} from "./elements-library/ElementsMenu.js";
+// import {hideElementsMenu} from "./elements-library/animation.js";
+//
+// import * as canvas from "./canvas/Canvas.js"
+// import {setImage as setCanvasImage} from "./canvas/setImage.js";
+//
+// import {AttributeEditor} from "./attribute-editor/AttributeEditor.js"
+// import {SizeSlider} from "./attribute-editor/ElementSizeSlider.js"
+//
+// import {BottomMenu} from "./buttons/BottomMenu.js";
+//
+//
+// // Loads element-library with elements, and sets click events on each element to show them on canvas
+// ElementsMenu.loadElements(element => {
+//     canvas.addElement(element)
+//     hideElementsMenu()  // hide menu when an user adds element
+// })
+//
+// setCanvasImage() // Sets the canvas image
+//
+// AttributeEditor() // Initializes the attribute-editor menu
+// SizeSlider(canvas.imageWidth) // Initializes size-slider
+//
+// BottomMenu() // Initializes buttons of bottom menu
