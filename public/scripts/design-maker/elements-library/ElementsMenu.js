0a45abd6fd12de95276e8586b2ee187dc3d6a363
nikolasaratlija <saratlijanikola@gmail.com>
2021-12-09 12:48:07 +0100

    removed await, just standard fetch now

--- public/scripts/design-maker/elements-library/ElementsMenu.js
+++ public/scripts/design-maker/elements-library/ElementsMenu.js
@@ -2,42 +2,41 @@ export const ElementsMenu = {
     loadElements: loadElements
 }
 
-const response = await fetch('api/elements')
-const files = await response.json()
-
 function loadElements(elementOnClick) {
-    files.forEach(file => {
-        // creates HTML imag element
-        let image = document.createElement('img')
-        image.src = 'assets/elements/' + file
+    fetch('api/elements')
+        .then(res => res.json())
+        .then(files => files.forEach(file => {
+            // creates HTML imag element
+            let image = document.createElement('img')
+            image.src = 'assets/elements/' + file
 
-        const filePrefix = file.split('-')[0]
+            const filePrefix = file.split('-')[0]
 
-        let menu;
-        // Switch case for each theme. Looks at prefix of file and appends image element to correct menu
-        switch (filePrefix) {
-            case 'g':   // Thema Groen
-                menu = document.getElementById('menu-groen')
-                break
-            case 'ss':  // Thema Spel & Sport
-                menu = document.getElementById('menu-spelsport')
-                break
-            case 'v':   // Thema Veilig
-                menu = document.getElementById('menu-veilig')
-                break
-            case 'o':   // Thema Ontspanning
-                menu = document.getElementById('menu-ontspanning')
-                break
-            default:    // No/Unrecognized prefix
-                console.error(`Prefix of '${filePrefix}' in file-name '${file}' not recognized.`)
-                break
-        }
+            let menu;
+            // Switch case for each theme. Looks at prefix of file and appends image element to correct menu
+            switch (filePrefix) {
+                case 'g':   // Thema Groen
+                    menu = document.getElementById('menu-groen')
+                    break
+                case 'ss':  // Thema Spel & Sport
+                    menu = document.getElementById('menu-spelsport')
+                    break
+                case 'v':   // Thema Veilig
+                    menu = document.getElementById('menu-veilig')
+                    break
+                case 'o':   // Thema Ontspanning
+                    menu = document.getElementById('menu-ontspanning')
+                    break
+                default:    // No/Unrecognized prefix
+                    console.error(`Prefix of '${filePrefix}' in file-name '${file}' not recognized.`)
+                    break
+            }
 
-        menu.appendChild(image) // Append image to correct menu
+            menu.appendChild(image) // Append image to correct menu
 
-        // onclick, adds this image to the canvas
-        image.addEventListener('click', (e) => elementOnClick(e.target))
-    })
+            // onclick, adds this image to the canvas
+            image.addEventListener('click', (e) => elementOnClick(e.target))
+        }))
 }
 
 
