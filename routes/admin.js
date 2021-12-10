1168353c24c4caac0931ac115f569d0662ddf9cf
nikolasaratlija <saratlijanikola@gmail.com>
2021-12-10 23:57:48 +0100

    added base for admin pages

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,31 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('admin/login')
+    res.render('admin/login', {
+        title: 'Admin Login',
+        bodyClass: 'login'
+    })
 })
 
-router.get('/dashboard', (req, res) => {
-    res.render('admin/dashboard')
+router.get('/locaties-dashboard', (req, res) => {
+    res.render('admin/locations-dashboard', {
+        title: 'Locaties Dashboard',
+        bodyClass: 'admin'
+    })
 })
+
+router.get('/elementen-dashboard', (req, res) => {
+    res.render('admin/elements-dashboard', {
+        title: 'Elementen Dashboard',
+        bodyClass: 'admin'
+    })
+})
+
+router.get('/ontwerpen-overzicht', ((req, res) => {
+    res.render('admin/designs-dashboard', {
+        title: 'Ontwerpen Dashboard',
+        bodyClass: 'admin'
+    })
+}))
 
 module.exports = router
