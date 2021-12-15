05b289b6f4f7087422ef8cb9b7df919d9ebd5c40
nikolasaratlija <saratlijanikola@gmail.com>
2021-12-15 08:33:07 +0100

    new database

--- database/mirotterdam.sql
+++ database/mirotterdam.sql
@@ -1,5 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 15, 2021 at 08:32 AM
+-- Server version: 10.6.5-MariaDB
+-- PHP Version: 7.3.31
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mirotterdam`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `elements`
+--
+
+CREATE TABLE `elements` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `width` int(11) NOT NULL,
+  `position_x` int(11) NOT NULL,
+  `position_y` int(11) NOT NULL,
+  `design_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `elements`
+--
+
+INSERT INTO `elements` (`id`, `name`, `width`, `position_x`, `position_y`, `design_id`) VALUES
+(1, 'g-Bloem1.png', 100, 58, 407, 5),
+(2, 'g-Bloem2.png', 100, 215, 255, 5),
+(3, 'g-Bloem1.png', 100, 215, 255, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `locations`
+--
 
 CREATE TABLE `locations` (
   `id` int(11) NOT NULL,
@@ -7,15 +57,59 @@ CREATE TABLE `locations` (
   `image` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `locations`
+--
+
 INSERT INTO `locations` (`id`, `location`, `image`) VALUES
-(5, 'Beijerlandselaand', 'L-5.png'),
-(7, 'Retiefstraat', 'L-6.png'),
+(5, 'Beijerlandselaand', 'L-5.webp'),
+(7, 'Retiefstraat', 'L-6.webp'),
 (16, 'Beurs', NULL);
 
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indexes for table `elements`
+--
+ALTER TABLE `elements`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `location_elements_fk` (`design_id`);
+
+--
+-- Indexes for table `locations`
+--
 ALTER TABLE `locations`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT for table `elements`
+--
+ALTER TABLE `elements`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `locations`
+--
 ALTER TABLE `locations`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `elements`
+--
+ALTER TABLE `elements`
+  ADD CONSTRAINT `location_elements_fk` FOREIGN KEY (`design_id`) REFERENCES `locations` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
