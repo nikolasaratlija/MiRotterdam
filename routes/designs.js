2d3f4b18a373612782c4b351f3f4fb6ea1dbe44a
nikolasaratlija <saratlijanikola@gmail.com>
2021-12-20 17:59:29 +0100

    /api/designs now correctly fetches all designs, including the location id

--- routes/designs.js
+++ routes/designs.js
@@ -3,12 +3,14 @@ const router = express.Router()
 const path = require('path')
 const conn = require('../database/connection')
 
-
+// get all designs, includes the location id and elements
 router.get('/', (req, res) => {
     conn.query(
-        `SELECT l.id AS location_id, e.image AS element_name, width, position_x, position_y
+        `SELECT e.image AS element_name, e.width, e.position_x, e.position_y, e.design_id, d.location_id
          FROM elements e
-                  JOIN locations l ON l.id = e.location_id`,
+                  INNER JOIN designs d ON d.id = e.design_id
+                  INNER JOIN locations l ON l.id = d.location_id
+        ORDER BY d.location_id`,
         (err, rows) => {
             if (err) res.send(err)
             else res.send(reduceData(rows))
@@ -43,18 +45,19 @@ router.post('/', ((req, res) => {
 function reduceData(designs) {
     let groupedDesigns = {}
 
-    // i have no clue how this works
-    designs.forEach(({location_id, element_name, width, position_x, position_y}) => {
-        groupedDesigns[location_id] = groupedDesigns[location_id] || {location_id, elements: []}
+    // I have no clue why this works
+    designs.forEach(({location_id, element_name, width, position_x, position_y, design_id}) => {
+        console.log(element_name)
+        groupedDesigns[design_id] = groupedDesigns[design_id] || {design_id, location_id, elements: []}
 
         const element = {
             'element_name': element_name,
             'width': width,
             'position_x': position_x,
-            'position_y': position_y
+            'position_y': position_y,
         }
 
-        groupedDesigns[location_id].elements.push(element)
+        groupedDesigns[design_id].elements.push(element)
     })
 
     return Object.values(groupedDesigns)
