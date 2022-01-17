86dae86a1e4db685f39603dc6cb0c787ac3e13ac
nikolasaratlija <saratlijanikola@gmail.com>
2022-01-18 00:31:27 +0100

    added comment

--- routes/elements.js
+++ routes/elements.js
@@ -5,13 +5,11 @@ const fs = require('fs')
 const path = require('path');
 const conn = require("../database/connection");
 
-
-
+// retrieves an array of all elements in specified folder
 router.get('/', (req, res) => {
     fs.readdir(path.join(__dirname, '../public/assets/elements'), (err, files) => {
         res.send(files)
     })
-    
 })
 
 module.exports = router
