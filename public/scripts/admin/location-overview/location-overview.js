b113bda422363bd4088099e4600e49146f351003
nikolasaratlija <saratlijanikola@gmail.com>
2022-01-18 20:15:05 +0100

    removed unused html

--- public/scripts/admin/location-overview/location-overview.js
+++ public/scripts/admin/location-overview/location-overview.js
@@ -81,8 +81,8 @@ imageWrapper.appendChild(image)
 card.appendChild(cardBody)
 cardBody.appendChild(locationText)
 cardBody.appendChild(buttonBar)
-buttonBar.appendChild(buttonGroup)
-buttonGroup.appendChild(CreateDeleteButton('Verwijder', 'btn-danger'))
+// buttonBar.appendChild(buttonGroup)
+// buttonGroup.appendChild(CreateDeleteButton('Verwijder', 'btn-danger'))
 
 
 }
